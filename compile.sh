771d8a4c40b3555c88e032ed45e749f148eeb22c
Freddy Uthup <freddy@freddy-lp.wgtn.cat-it.co.nz>
2015-06-05 14:33:25 +1200

    Added some comments to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Demonstrates how to compile the documentation for local testing
 
+# Create a Python virtual environment
 virtualenv venv
 source venv/bin/activate
+
+# Install the Python requirements on the virtual environment
 pip install -r requirements.txt
+
+# Compile the documentation
 make html
