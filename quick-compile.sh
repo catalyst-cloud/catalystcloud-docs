d4cad4ffc3e90b4fefc63a324d8f3c8c0d3fdb89
Glyn Davies <glyndavies@catalyst.net.nz>
2020-09-25 12:43:23 +1200

    update compile files

--- quick-compile.sh
+++ quick-compile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Compile the documentation
-make html
+if [[ -d venv ]]; then
+  # Activate the virtual environment
+  source venv/bin/activate
+  # Compile the documentation
+  make html
+else
+  echo "The virtual environment venv does not exist, please run ./compile.sh instead."
+fi
