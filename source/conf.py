8dd622b3e9dd3eaa6e15ab3dc1863abf93e13cf6
Daniel O'Byrne <danielobyrne@catalyst.net.nz>
2021-03-19 09:27:10 +1300

    updated the copyright lisence at the bottom of the docs to include 2021 and say Catalyst Cloud Ltd

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Catalyst Cloud'
-copyright = u'2016-2020, Catalyst IT'
+copyright = u'2016-2021, Catalyst Cloud Ltd'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
